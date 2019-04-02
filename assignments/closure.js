446441c795c50e2c7d5ab81c5d9a3b634fa4af77
joerlop <jj.erlich155@gmail.com>
2019-04-02 15:32:22 -0500

    Stretch: Completed closure.js stretch

--- assignments/closure.js
+++ assignments/closure.js
@@ -28,14 +28,43 @@ yourMajesty();
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let accumulator = 0;
+  return function() {
+    return ++accumulator;
+  }
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let accumulator = 0;
+  
+  return {
+    "increment": function() {
+      return ++accumulator;
+    },
+
+    "decrement": function() {
+      return --accumulator;
+    }
+  }
 };
+
+const newCounterFactory = counterFactory();
+console.log(newCounterFactory.increment());
+console.log(newCounterFactory.increment());
+console.log(newCounterFactory.increment());
+console.log(newCounterFactory.increment());
+console.log(newCounterFactory.decrement());
+console.log(newCounterFactory.decrement());
